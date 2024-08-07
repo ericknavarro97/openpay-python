761755f38bba7dc6951f7ef9e6d854ae12474084
Erick Navarro <thenavarro97@gmail.com>
2024-08-07 13:42:48 -0600

    import six package and update fix

--- openpay/util.py
+++ openpay/util.py
@@ -1,8 +1,7 @@
-
 import logging
 import sys
 
-from past.builtins import unicode
+import six
 
 logger = logging.getLogger('stripe')
 
@@ -10,7 +9,7 @@ __all__ = ['utf8']
 
 
 def utf8(value):
-    if isinstance(value, unicode) and sys.version_info < (3, 0):
+    if isinstance(value, six.text_type) and sys.version_info < (3, 0):
         return value.encode('utf-8')
     else:
         return value
